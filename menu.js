b378295b0a414d1908061478566a14697052e99a
Naiara <naiaraantunes.design@gmail.com>
2024-11-12 15:28:13 -0300

    Atualização Portfólio

--- menu.js
+++ menu.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
       infinite: true, 
       slidesToShow: 3, 
       slidesToScroll: 1, 
-      arrows: true, 
+      arrows: false, 
       dots: true, 
       responsive: [
         {
