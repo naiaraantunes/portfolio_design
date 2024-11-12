8d2d1fc74f4ac782585c96b103e621733435ee90
Naiara <naiaraantunes.design@gmail.com>
2024-11-12 14:11:19 -0300

    Ajustes Carrossel

--- menu.js
+++ menu.js
@@ -14,4 +14,38 @@ overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
 
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+  
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+$(document).ready(function(){
+    $('.carrossel').slick({
+      infinite: true, 
+      slidesToShow: 3, 
+      slidesToScroll: 1, 
+      arrows: true, 
+      dots: true, 
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+      ]
+    });
+  });
+  
   
